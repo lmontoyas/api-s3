9275ab198d03f7090800fd2be629603aac8ba665
lmontoyas <83974237+lmontoyas@users.noreply.github.com>
2024-11-03 22:41:43 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -3,18 +3,9 @@ import boto3
 def lambda_handler(event, context):
     nombre_bucket = event['body']['nombreBucket']
 
-    try:
-        s3 = boto3.client("s3")
-        s3.create_bucket(Bucket = nombre_bucket)
-        
-        return {
-            'statusCode': 201,
-            'message': f"{nombre_bucket} creado exitosamente"
-        }
-    
-    except Exception as e:
-        print(e)
-        return {
-            'statusCode': 400,
-            'message': f"Error al crear el bucket: {str(e)}"
-        }
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket=nombre_bucket)
+    return {
+        'statusCode': 201,
+        'message': f"{nombre_bucket} creado exitosamente"
+    }
